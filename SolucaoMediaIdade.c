1ebfa5a2d0642eeb15db11ece7aa74b9e59370aa
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-02 20:49:02 -0300

    Solução do problema media idades proposto pelo instrutor com laço de repetição que se a variavel for negativo o laço de repetição para. Ou seja, se a idade recebida pela a variavel for negativa a condição para. Do contrario se repete ate ser negativo!

---
+++ SolucaoMediaIdade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main () {
+
+    int idade, cont, soma;
+    double media;
+
+    printf("Digite as idades:\n");
+    scanf("%d", &idade);
+
+    soma = 0;
+    cont = 0;
+
+    while (idade >= 0) {
+        soma = soma + idade;
+        cont = cont + 1;
+        scanf("%d", &idade);
+    }
+
+    if (cont == 0) {
+        printf("Impossivel de calcular!");
+    } else {
+        media = (double)soma / cont;
+        printf("Media = %.2lf", media);
+    }
+    return 0;
+}
