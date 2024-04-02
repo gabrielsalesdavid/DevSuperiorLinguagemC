b2c58ed87126e00990141c470e25e8f9e5d8d404
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-02 19:15:37 -0300

    Solução do problema retangulo proposto pelo instrutor!

---
+++ SolucaoIdade.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+
+    char nome01[50], nome02[50];
+    int idade01, idade02;
+    double media;
+
+    printf("Dados da primeira pessoa: ");
+    printf("\nNome: ");
+    gets(nome01);
+    printf("Idade: ");
+    scanf("%d", &idade01);
+
+    printf("\nDados da segunda pessoa: ");
+    printf("\nNome: ");
+    fseek(stdin, 0, SEEK_END);
+    gets(nome02);
+    printf("Idade: ");
+    scanf("%d", &idade02);
+
+    media = (idade01 + idade02) / 2.0;
+
+    printf("A idade media de %s e %s eh de %.1lf anos", nome01, nome02, media);
+
+    return 0;
+}
