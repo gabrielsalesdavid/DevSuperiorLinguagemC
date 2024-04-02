1f04c37ee287be4d6724f2dbd39d1ae0f1eebf42
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-02 19:52:58 -0300

    Solução do problema Baskara proposto pelo instrutor com a estrutura condicional composta. Com retorno de dados tanto da nota final quanto por mensagem!

--- SolucaoBaskara.c
+++ SolucaoBaskara.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 int main () {
 
+    double a, b, c, delta, x1, x2;
 
+    printf("Coeficiente A: ");
+    scanf("%lf", &a);
+    printf("Coeficiente B: ");
+    scanf("%lf", &b);
+    printf("Coeficiente C: ");
+    scanf("%lf", &c);
 
+    delta = b * b - 4 * a * c;
+
+    if (a == 0 || delta < 0) {
+        printf("\nEsta equacao nao possui raizes rais");
+    } else {
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("\nX1 = %.4lf", x1);
+        printf("\nX2 = %.4lf", x2);
+    }
     return 0;
 }
